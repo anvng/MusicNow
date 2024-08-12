4d82090b0ca9d8db6e7b28f964074c1d16a63bce
An Van Nguyen <152575252+anvng@users.noreply.github.com>
2024-08-12 15:39:46 +0700

    feat: validate email and password

--- client/music_now/lib/features/auth/view/widgets/custom_field.dart
+++ client/music_now/lib/features/auth/view/widgets/custom_field.dart
@@ -26,6 +26,19 @@ class _CustomFieldState extends State<CustomField> {
         if (value!.trim().isEmpty) {
           return "$widget.hintText is Missing!";
         }
+
+        if (widget.hintText.toLowerCase() == "email") {
+          // validate
+          const emailPattern =
+              r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$';
+          final regex = RegExp(emailPattern);
+          if (!regex.hasMatch(value)) {
+            return "Please enter a valid email address";
+          }
+        }
+        if (widget.hintText == 'Password' && value.length < 6) {
+          return 'Password must be at least 6 characters';
+        }
         return null;
       },
       obscureText: widget.isobscureText,
