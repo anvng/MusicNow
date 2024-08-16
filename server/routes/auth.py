1a7b6408fd1fc4d15ea0cae4e8307bf7da109bcd
An Van Nguyen <152575252+anvng@users.noreply.github.com>
2024-08-16 15:43:31 +0700

    feat: login API route

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -5,12 +5,13 @@ from fastapi import APIRouter # type: ignore
 from pydantic_schemas.user_create import UserCreate
 from models.user import User
 from database import get_db
+from pydantic_schemas.user_login import UserLogin
 from sqlalchemy.orm import Session # type: ignore
 
 
 router = APIRouter()
 
-@router.post('/register')
+@router.post('/register', status_code=201)
 def register_user(user: UserCreate, db: Session = Depends(get_db)):
     user_db = db.query(User).filter_by(email=user.email).first()
 
@@ -22,5 +23,18 @@ def register_user(user: UserCreate, db: Session = Depends(get_db)):
     db.add(user_db)
     db.commit()
     db.refresh(user_db)
+
+    return user_db
+
+@router.post('/login')
+def login_user(user: UserLogin, db: Session = Depends(get_db)):
+    user_db = db.query(User).filter_by(email=user.email).first()
+
+    if not user_db:
+        raise HTTPException(404, "User not found!")
+
+    if not bcrypt.checkpw(user.password.encode('utf-8'), user_db.password):
+        raise HTTPException(401, "Wrong password!")
+    
 
     return user_db
